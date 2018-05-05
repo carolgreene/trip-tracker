fd786fbe15e3ac625cd246931d9765ba7c2a1414
Carol Greene <carolgreene3@gmail.com>
2018-05-05 17:37:18 +0000

    added post signup & views to file tree

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,19 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-      erb :'users/create_user'
+      erb :'users/new'
     else
+      redirect '/trips'
+    end
+  end
+
+  post '/signup' do
+    if params["username"] == "" || params["email"] == "" ||  params["password"] == ""
+      redirect '/signup'
+    else
+      @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+      @user.save
+      session[:user_id] = @user.id
       redirect '/trips'
     end
   end
