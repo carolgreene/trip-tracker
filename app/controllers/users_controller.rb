62f7d8937a9a9d4138ece7d91a79620e57a9524c
Carol Greene <carolgreene3@gmail.com>
2018-05-05 16:27:25 +0000

    added helpers & signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect '/trips'
+    end
+  end
+
+
+
+
 end
