caa6b85618d76eb2378427864fbd1555749f8bd8
Carol Greene <carolgreene3@gmail.com>
2018-05-05 18:42:02 +0000

    added trips/new & get trips to controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,3 +1,20 @@
 class TripsController < ApplicationController
 
+  get 'trips/new' do
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'/trips/create_trip'
+    end
+  end
+
+  get '/trips' do
+    if !logged_in?
+      redirect '/login'
+    else
+      @user = User.find(session[:user_id])
+      erb :'/trips/trips'
+    end
+  end
+
 end
