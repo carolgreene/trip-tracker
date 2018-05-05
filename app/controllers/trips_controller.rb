2611e5a847abe58d3cf0ffd99768fadc2cf3757c
Carol Greene <carolgreene3@gmail.com>
2018-05-05 18:53:21 +0000

    added post /trips & get /trips/:id to controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -17,4 +17,24 @@ class TripsController < ApplicationController
     end
   end
 
+  post '/trips' do
+    if !params[:trip_name].empty? && !params[:description].empty?
+      @trip = Trip.create(trip_name: params["trip_name"], description: params["description"])
+      @trip.user_id = session[:user_id]
+      @trip.save
+      erb :'trips/trips'
+    else
+      redirect '/trips/new'
+    end
+  end
+
+  get '/trips/:id' do
+    if logged_in?
+      @trip = Trip.find_by_id(params[:id])
+      erb :'/trips/show_trip'
+    else
+      redirect '/login'
+    end
+  end
+
 end
