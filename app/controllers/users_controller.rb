0b82ecc1b238e6cab934dc12964066ffbe111dc8
Carol Greene <carolgreene3@gmail.com>
2018-05-11 20:29:51 +0000

    fixed error msgs for user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,13 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    if params["username"] == "" || params["email"] == "" ||  params["password"] == ""
-      redirect '/signup'
-    else
-      @user = User.new(username: params["username"], email: params["email"], password: params["password"])
-      @user.save
+    @user = User.new(username: params["username"], email: params["email"], password: params["password"])
+    if @user.save
       session[:user_id] = @user.id
       redirect '/trips'
+    else
+      flash[:message] = @user.errors.full_messages.join("<br>")
+      erb :'/users/create_user'
     end
   end
 
